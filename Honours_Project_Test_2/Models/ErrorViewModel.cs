95ca83a14f57e6e471ee5396d2e2c8c540cc9048
rtaggart16 <rosstaggart44@gmail.com>
2020-02-12 19:23:36 +0000

    Add project files.

---
+++ Honours_Project_Test_2/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Honours_Project_Test_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
